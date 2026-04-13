9030473938946d71f2da8d787a4f1ae63045d764
electrovir <electrovir@users.noreply.github.com>
2026-04-13 16:08:24 +0000

    [patch] use device scale

--- packages/pdf-vir/src/pdf-vir.element.ts
+++ packages/pdf-vir/src/pdf-vir.element.ts
@@ -237,10 +237,13 @@ export const PdfVir = defineElement<PdfVirInputs>()({
                             assert.instanceOf(canvas, HTMLCanvasElement);
 
                             const pdfPage = await pdfDocument.getPage(pageNumber);
-                            const viewport = pdfPage.getViewport({scale: 1});
+                            const dpr = globalThis.devicePixelRatio || 1;
+                            const viewport = pdfPage.getViewport({scale: dpr});
 
                             canvas.width = viewport.width;
                             canvas.height = viewport.height;
+                            canvas.style.width = `${viewport.width / dpr}px`;
+                            canvas.style.height = `${viewport.height / dpr}px`;
                             const context = canvas.getContext('2d');
 
                             assert.isDefined(context);
